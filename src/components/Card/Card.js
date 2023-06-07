9b23b7bc48742c3180403aab72f8c4d3ab90afd1
Sidhiq Ali <sidhiqsidhiqali@gmail.com>
2023-06-08 00:21:10 +0530

    first commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import "./Card.css"
-import axios from '../../axios'
-import { imgUrl,API_KEY } from '../constants/constant'
-import YouTube from 'react-youtube'
+import React, { useEffect, useState } from 'react';
+import './Card.css';
+import axios from '../../axios';
+import { imgUrl, API_KEY } from '../constants/constant';
+import YouTube from 'react-youtube';
 
 function Card(props) {
-  const [movie, setmovie] = useState([])
-  const [vidId, setvidId] = useState()
+  const [movie, setmovie] = useState([]);
+  const [vidId, setvidId] = useState();
   useEffect(() => {
-    axios.get(props.url).then((response)=>{
-      setmovie(response.data.results)
-      
-    })
-  }, [])
+    axios.get(props.url).then((response) => {
+      setmovie(response.data.results);
+    });
+  }, [props.url]);
 
-  const HandleTrailer = (id)=>{
+  const HandleTrailer = (id) => {
     console.log(id);
-    axios.get(`movie/${id}/videos?api_key=${API_KEY}`).then((response)=>{
-      if(response.data.results.length!==0){
-        setvidId(response.data.results[0])
-      }else{
-        alert("trailer not available")
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}`).then((response) => {
+      if (response.data.results.length !== 0) {
+        setvidId(response.data.results[0]);
+      } else {
+        alert('trailer not available');
       }
-      
-    })
-  }
+    });
+  };
 
   const opts = {
     height: '390',
@@ -34,24 +32,23 @@ function Card(props) {
       autoplay: 1,
     },
   };
-  
+
   return (
     <div className='row'>
-            <h2>{props.title}</h2>
-            <div className='posters'>
-            
-            {
-              movie.map((obj)=>
-            
-                <img onClick={()=>HandleTrailer(obj.id)} className='poster' alt='poster' src={`${imgUrl+obj.backdrop_path}`} />
-              
-              )
-            }
-             
-            </div>
-            {vidId && <YouTube opts={opts} videoId={vidId.key} />}
-        </div>
-  )
+      <h2>{props.title}</h2>
+      <div className='posters'>
+        {movie.map((obj) => (
+          <img
+            onClick={() => HandleTrailer(obj.id)}
+            className='poster'
+            alt='poster'
+            src={`${imgUrl + obj.backdrop_path}`}
+          />
+        ))}
+      </div>
+      {vidId && <YouTube opts={opts} videoId={vidId.key} />}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
